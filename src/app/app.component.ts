6b921018d3d66a60eb1878f42225ab59984e49b1
MrTGK <mrtiti30@gmail.com>
2024-09-17 14:07:11 +0200

    Add : folders in app folder
    Modif : ajout httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './pages/app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {

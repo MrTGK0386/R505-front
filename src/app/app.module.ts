2002c033444c6e20848dcc5843e490cb51ea4344
MrTGK <mrtiti30@gmail.com>
2024-09-17 15:04:20 +0200

    Add : Pokemon-list component Pokemone-card component (layout) + SCSS
    Modif : Routes for the new components
    New CSS Class for cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {provideHttpClient} from '@angular/common/http';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './shared/layouts/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,

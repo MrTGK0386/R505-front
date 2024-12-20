94a1b8a2ea2dcf59d264e1424d6ece88f47c6c09
MrTGK <mrtiti30@gmail.com>
2024-12-20 10:51:00 +0100

    ADD : ajout du code pour les variantes de header

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isWindowSmall : boolean = false;
+  private intervallId!: number;
+
+  ngOnInit() {
+    this.TestWindowSize();
+    this.intervallId = window.setInterval(() => {
+      this.TestWindowSize();
+    },1000);
+  }
+
+  TestWindowSize(){
+    if (window.innerWidth <= 767){
+      this.isWindowSmall = true;
+    }
+    else {
+      this.isWindowSmall = false;
+    }
+    return this.isWindowSmall;
+  }
+
+  isDarkMode = false;
+
+  onBack(): void {
+    return;
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    // Ajoutez ici la logique pour changer le thème
+  }
+
+  onSearch(): void {
+    // Logique pour la recherche
+  }
+
+  onGridView(): void {
+    // Logique pour changer la vue
+  }
+
+  ngOnDestroy() {
+    if (this.intervallId) {
+      clearInterval(this.intervallId);
+    }
+  }
+
 
 }
